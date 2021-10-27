9bbc0211e22209650eb1bba2fd488cd516372b0e
ArtificialButter <linus.stotts@gmail.com>
2021-10-26 21:38:23 -0700

    fixi

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    app: {
+        px: 't.',
+        token: '',
+        playing: 'discord.io/tunez | t.help  1'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {}
+    }
+};
