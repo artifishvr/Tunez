6c497b99893574a7b25f79147798a54aae0e1206
ArtificialButter <linus.stotts@gmail.com>
2021-12-02 14:10:59 -0800

    Added status command, and updated README

---
+++ commands/status.js
@@ -0,0 +1,22 @@
+const { SlashCommand } = require('slash-create');
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'status',
+            description: 'Gets the status of Tunez',
+
+            guildIDs: process.env.DISCORD_GUILD_ID ? [ process.env.DISCORD_GUILD_ID ] : undefined
+        });
+    }
+
+    async run (ctx) {
+
+        const { client } = require('..');
+
+        await ctx.defer();
+// 
+
+        ctx.sendFollowUp({ content: `💻 | Currently in **${client.guilds.cache.size}** servers\n📶 | Ping: **${client.ws.ping}**` });
+    }
+};
