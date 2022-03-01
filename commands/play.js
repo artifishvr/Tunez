81e9b3848e6351d3cc7ae0bf0359d6582b535f50
ArtificialButter <linus.stotts@gmail.com>
2022-03-01 00:00:33 -0800

    YTDL fixes

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,5 @@
 const { SlashCommand, CommandOptionType } = require('slash-create');
 const { QueryType } = require('discord-player');
-const playdl = require("play-dl");
 
 module.exports = class extends SlashCommand {
     constructor(creator) {
@@ -40,38 +39,17 @@ module.exports = class extends SlashCommand {
         if (!searchResult || !searchResult.tracks.length) return void ctx.sendFollowUp({ content: 'No results were found!' });
 
         const queue = await client.player.createQueue(guild, {
-            metadata: channel,
-            // use play-dl
-            async onBeforeCreateStream(track, source, _queue) {
-                if (track.url.includes('youtube') || track.url.includes("youtu.be")) {
-                    try {
-                        return (await playdl.stream(track.url)).stream;
-                    } catch (err) {
-                        return _queue.metadata.m.errorMessage("This video is restricted. Try with another link.")
-                    }
-                } else if (track.url.includes('spotify')) {
-                    try {
-                        let songs = await client.player.search(`${track.author} ${track.title} `, {
-                            requestedBy: message.member,
-                        }).catch().then(x => x.tracks[0]);
-                        return (await playdl.stream(songs.url)).stream;
-                    } catch (err) {
-                        console.log(err)
-                    }
-                } else if (track.url.includes('soundcloud')) {
-                    try {
-                        return (await playdl.stream(track.url)).stream;
-                    } catch (err) {
-                        console.log(err)
-                    }
-                }
-            }
+            ytdlOptions: {
+                filter: 'audioonly',
+                highWaterMark: 1 << 30,
+                dlChunkSize: 0,
+            },
+            metadata: channel
         });
 
         const member = guild.members.cache.get(ctx.user.id) ?? await guild.members.fetch(ctx.user.id);
         try {
             if (!queue.connection) await queue.connect(member.voice.channel);
-            
         } catch {
             void client.player.deleteQueue(ctx.guildID);
             return void ctx.sendFollowUp({ content: 'Could not join your voice channel!' });
