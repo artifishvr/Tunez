010c97c8a6d078bd6a3d859593f3fc4b1f59766d
ArtificialButter <59352535+artificialbutter@users.noreply.github.com>
2022-03-28 13:05:38 -0700

    Add Lyrics Finding!
    I wrote most of that code using github copilot :D

---
+++ commands/lyrics.js
@@ -0,0 +1,40 @@
+const { SlashCommand } = require('slash-create');
+const { Lyrics } = require("@discord-player/extractor");
+const lyricsClient = Lyrics.init();
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'lyrics',
+            description: 'See lyrics for the current song',
+
+            guildIDs: process.env.DISCORD_GUILD_ID ? [ process.env.DISCORD_GUILD_ID ] : undefined
+        });
+    }
+
+    async run (ctx) {
+
+        const { client } = require('..');
+
+        await ctx.defer();
+
+        const queue = client.player.getQueue(ctx.guildID);
+        if (!queue || !queue.playing) return void ctx.sendFollowUp({ content: '❌ | No music is being played!' });
+
+            const lyrics = await lyricsClient.search(queue.current.title, queue.current.artist);
+            if (!lyrics) return void ctx.sendFollowUp({ content: '❌ | No lyrics found!' });
+            return void ctx.sendFollowUp({
+              embeds: [
+                {
+                  title: lyrics.artist.name + " - " + lyrics.title,
+                  description: lyrics.lyrics,
+                  color: 0xffffff,
+                },
+              ],
+            });
+
+        // thanks copilot
+
+        
+    }
+};
